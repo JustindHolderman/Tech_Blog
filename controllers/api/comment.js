eb283a60a800b4037ecc52d1ad9dd12fc3f61bbc
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-05 16:19:01 -0500

    Added post and index routes under API folder.

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -1,1 +1,9 @@
-// Placeholder
+const express = require('express');
+const router = express.Router();
+const { Comment } = require('../../models');
+
+router.get('/', async (req, res) => {
+    res.json('Comment route');
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,13 @@
-// Placeholder
+const express = require('express');
+const router = express.Router();
+
+const userRoutes = require('./user');
+const postRoutes = require('./post');
+const commentRoutes = require('./comment');
+
+router.use('/posts', postRoutes);  
+router.use('/comments', commentRoutes);
+router.use('/users', userRoutes);
+
+
+module.exports = router;
