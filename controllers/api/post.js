503f92e9000ef9d330350a96848a34805d68c9f8
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-05 16:17:53 -0500

    added post.js routing in API folder.

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -1,1 +1,9 @@
-// Placeholder
+const express = require('express');
+const router = express.Router();
+const {Post} = require('../../models')
+
+router.get('/', async (req, res) => {
+    req.sessionID('Posted');
+});
+
+module.exports = router;
