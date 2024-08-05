34073858d8b9c38d1662dfa70c9f2b10aa4fd3be
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-05 16:12:56 -0500

    User.js routing updated.

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -1,1 +1,34 @@
-// Placeholder
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const { User } = require('../../models/index');
+
+router.get('/register', async (req, res) => {
+    res.render('register');
+});
+
+router.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+
+    if (!username || !email || !password) {
+        res.status(400).json({ message: 'All fields must be filled' });
+        return;
+    }
+
+    try {
+        const hashedPass = await bcrypt.hash(password, 10);
+        const user = await User.create({ username, email, password: hashedPass });
+        req.session.userId = user.id;
+        req.session.user = user;
+        req.session.loggedIn = true;
+
+        res.redirect('/');
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+
+});
+
+module.exports = router;
