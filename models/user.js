e2987f5ebe2217ef91b5e863aa20033a50a2e8d4
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-05 16:14:33 -0500

    Added the setup of user.js within the models.

--- models/user.js
+++ models/user.js
@@ -1,1 +1,10 @@
-// Placeholder
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
